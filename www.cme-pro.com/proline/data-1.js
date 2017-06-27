afe0ae462c723fb796e9773955180f1030b53043
orionbytes <mail@orion-bytes.com>
2017-06-27 12:50:35 +0200

    VX5 (first version as I know) firmwares I used VxBrain 1.1 Beta installed from www.cme-pro.com site (archive somewhere in folder) Windows XP (64bit) guest in Virtual Box on Windows 7 host to load firmwares. No good results from Windows 7 There is also mac version of the VxBrain  (archive somewhere in www.cme-pro.com folder) If you get in troubles during firmware loading - turn keyboard off and push UTILITY+EXPANSION keys during next turn-on - keyboard will automatically go to firmware loading mode and you will be able to try VxBrain again
    There are several manuals in www.cme-pro.com and materials for the other boards - try to look through.

---
+++ www.cme-pro.com/proline/data-1.js
@@ -0,0 +1,69 @@
+var title = "CME Artist - Interface"
+var subTitle = ""
+var URL = "interface.html"/*tpa=http://www.cme-pro.com/endorser/Interface/interface.html*/
+var BGCOLOR = "#000000"
+var TEXT = "#FFFFFF"
+var LINK = "#000000"
+var VLINK = "#141414"
+var ALINK = "#141414"
+var pageFontFamily = "font-family: Arial;"
+var pageFontSize = "font-size: medium;"
+var thumbFontFamily = "font-family: Arial;"
+var thumbFontSize = "font-size: medium;"
+var thumbFontColor = "color: #FFFFFF";
+var imageFontFamily = "font-family: Arial;"
+var imageFontSize = "font-size: medium;"
+var imageFontColor = "color: #FFFFFF";
+var rows = "1"
+var columns = "3"
+var thumbDB = new Array(
+"Thumb_interface1.jpg"/*tpa=http://www.cme-pro.com/endorser/Interface/pics/Thumb_interface1.jpg*/,
+"Thumb_interface2.jpg"/*tpa=http://www.cme-pro.com/endorser/Interface/pics/Thumb_interface2.jpg*/,
+"Thumb_interface3.jpg"/*tpa=http://www.cme-pro.com/endorser/Interface/pics/Thumb_interface3.jpg*/
+)
+
+var imageDB = new Array(
+"interface1.jpg"/*tpa=http://www.cme-pro.com/endorser/Interface/pics/interface1.jpg*/,
+"interface2.jpg"/*tpa=http://www.cme-pro.com/endorser/Interface/pics/interface2.jpg*/,
+"interface3.jpg"/*tpa=http://www.cme-pro.com/endorser/Interface/pics/interface3.jpg*/
+)
+var thumbMetadataDB = new Array(
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+""
+)
+var imageMetadataDB = new Array(
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+""
+)
